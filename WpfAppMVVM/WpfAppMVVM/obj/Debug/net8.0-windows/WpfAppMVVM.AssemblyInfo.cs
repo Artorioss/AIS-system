fa0fb995a61a6b1f6d509a08ac6d9ec473d4f1c5
Artorioss <102064880+Artorioss@users.noreply.github.com>
2024-05-13 16:04:03 +0300

    Временные файлы

--- WpfAppMVVM/WpfAppMVVM/obj/Debug/net8.0-windows/WpfAppMVVM.AssemblyInfo.cs
+++ WpfAppMVVM/WpfAppMVVM/obj/Debug/net8.0-windows/WpfAppMVVM.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WpfAppMVVM")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+79c00b3adbfc9d6502902aa8ab4891c886a3c870")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d068dcb04990dd4567aecaf6428b2fe6fc66bad4")]
 [assembly: System.Reflection.AssemblyProductAttribute("WpfAppMVVM")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WpfAppMVVM")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
