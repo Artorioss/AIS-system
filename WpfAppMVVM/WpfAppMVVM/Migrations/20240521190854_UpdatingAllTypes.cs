c56feb4ba44563ace6e8d421ed4147895fd87815
Artorioss <102064880+Artorioss@users.noreply.github.com>
2024-05-21 22:13:14 +0300

    Обновлена разметка окна для создания водителя

---
+++ WpfAppMVVM/WpfAppMVVM/Migrations/20240521190854_UpdatingAllTypes.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace WpfAppMVVM.Migrations
+{
+    /// <inheritdoc />
+    public partial class UpdatingAllTypes : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "DateLoading",
+                table: "Transportations",
+                type: "character varying(32)",
+                maxLength: 32,
+                nullable: true,
+                oldClrType: typeof(DateTime),
+                oldType: "Date");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "AccountDate",
+                table: "Transportations",
+                type: "character varying(32)",
+                maxLength: 32,
+                nullable: true,
+                oldClrType: typeof(DateTime),
+                oldType: "Date",
+                oldNullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<DateTime>(
+                name: "DateLoading",
+                table: "Transportations",
+                type: "Date",
+                nullable: false,
+                defaultValue: new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified),
+                oldClrType: typeof(string),
+                oldType: "character varying(32)",
+                oldMaxLength: 32,
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<DateTime>(
+                name: "AccountDate",
+                table: "Transportations",
+                type: "Date",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "character varying(32)",
+                oldMaxLength: 32,
+                oldNullable: true);
+        }
+    }
+}
---
+++ WpfAppMVVM/WpfAppMVVM/Model/EntityConfigurations/CarConfig.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WpfAppMVVM.Models.Entities;
+
+namespace WpfAppMVVM.Model.EntityConfigurations
+{
+    internal class CarConfig : IEntityTypeConfiguration<Car>
+    {
+        public void Configure(EntityTypeBuilder<Car> entity)
+        {
+            
+        }
+    } 
+}
