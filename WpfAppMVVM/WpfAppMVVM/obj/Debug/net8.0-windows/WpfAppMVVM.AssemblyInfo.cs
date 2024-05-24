c7fe892ef7210a0eff75e77f7f094aee302dc4b3
Artorioss <102064880+Artorioss@users.noreply.github.com>
2024-05-24 10:48:54 +0300

    Updating gitignore

--- WpfAppMVVM/WpfAppMVVM/obj/Debug/net8.0-windows/WpfAppMVVM.AssemblyInfo.cs
+++ WpfAppMVVM/WpfAppMVVM/obj/Debug/net8.0-windows/WpfAppMVVM.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WpfAppMVVM")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c56feb4ba44563ace6e8d421ed4147895fd87815")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f6c7b8035d25f5be1e032d5bfe66d1b1b1157601")]
 [assembly: System.Reflection.AssemblyProductAttribute("WpfAppMVVM")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WpfAppMVVM")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
